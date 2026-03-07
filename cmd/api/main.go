2ab568754bbc3ab3334e2cc7b5844ce1d2862574
Majd Taweel <42496516+MajdTaweel@users.noreply.github.com>
2026-03-08 01:02:58 +0200

    feat(users): send welcome email to new users

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -5,10 +5,12 @@ import (
 	"database/sql"
 	"flag"
 	"os"
+	"sync"
 	"time"
 
 	"greenlight.themt.tech/internal/data"
 	"greenlight.themt.tech/internal/jsonlog"
+	"greenlight.themt.tech/internal/mailer"
 
 	"github.com/joho/godotenv"
 	_ "github.com/lib/pq"
@@ -30,12 +32,21 @@ type config struct {
 		burst   int
 		enabled bool
 	}
+	smtp struct {
+		host     string
+		port     int
+		username string
+		password string
+		sender   string
+	}
 }
 
 type application struct {
 	config config
 	logger *jsonlog.Logger
 	models data.Models
+	mailer mailer.Mailer
+	wg     sync.WaitGroup
 }
 
 func main() {
@@ -61,6 +72,12 @@ func main() {
 	flag.IntVar(&cfg.limiter.burst, "limiter-burst", 4, "Rate limiter maximum burst")
 	flag.BoolVar(&cfg.limiter.enabled, "limiter-enabled", true, "Enable rate limiter")
 
+	flag.StringVar(&cfg.smtp.host, "smtp-host", "sandbox.smtp.mailtrap.io", "SMTP host")
+	flag.IntVar(&cfg.smtp.port, "smtp-port", 587, "SMTP port")
+	flag.StringVar(&cfg.smtp.username, "smtp-username", "a496b58bf76d1f", "SMTP username")
+	flag.StringVar(&cfg.smtp.password, "smtp-password", "070227b3e25b45", "SMTP password")
+	flag.StringVar(&cfg.smtp.sender, "smtp-sender", "Greenlight <no-reply@greenlight.themt.tech>", "SMTP sender")
+
 	flag.Parse()
 
 	db, err := openDB(cfg)
@@ -76,6 +93,7 @@ func main() {
 		config: cfg,
 		logger: logger,
 		models: data.NewModels(db),
+		mailer: mailer.New(cfg.smtp.host, cfg.smtp.port, cfg.smtp.username, cfg.smtp.password, cfg.smtp.sender),
 	}
 
 	err = app.serve()
