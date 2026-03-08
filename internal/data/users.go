1a42cc4b24168ce0c4032f4187b2d51aa0e239ef
Majd Taweel <42496516+MajdTaweel@users.noreply.github.com>
2026-03-09 00:08:09 +0200

    chore(auth): implement user auth via tokens

--- internal/data/users.go
+++ internal/data/users.go
@@ -12,6 +12,8 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -190,6 +192,10 @@ func (p *password) Matches(plaintextPassword string) (bool, error) {
 	}
 
 	return true, nil
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 func ValidateEmail(v *validator.Validator, email string) {
